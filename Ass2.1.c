167ed23538dfd26ec6223d8c3fbd74a1e47e197a
HithashreeB <154512550+HithashreeB@users.noreply.github.com>
2025-02-10 22:46:56 +0530

    Update Ass2.1.c

--- Ass2.1.c
+++ Ass2.1.c
@@ -1,3 +1,8 @@
+//You are developing a user registration system for a website. The username entered by the user must follow these rules:
+//It should contain only alphanumeric characters (a-z, A-Z, 0-9).
+//It should start with a letter (not a number or special character).
+//It should be at least 5 characters long and at most 15 characters long.
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
